478992980558fe8876d8680e44779b684d5db7e1
Ray Luong <rayluong@gmail.com>
2017-02-15 15:09:31 -0800

    draws a single hour

--- script.js
+++ script.js
@@ -24,79 +24,11 @@ function ready(error, coordinatesJSON, ridersJSON) {
   coordinates = coordinatesJSON;
   riders = ridersJSON;
   console.log("coordinates: " + JSON.stringify(coordinates));
-  massage();
   draw();
 }
-
-function massage() {
-  var h = 0;
-  // filter ridership data to only trips on a specific line (red, blue, green, etc)
-  var redLineRiders = riders.filter(isOnRed);
-  // filter ridership data to direction NB or SB
-  var redLineRidersSouthBound = redLineRiders.filter(isSouthBound);
-  var trips = redLineRidersSouthBound.filter(tripsHour(h));
-
-  function matchTrip(origin, dest, t) {
-    return t.origin == origin && t.dest == dest;
-  }
-
-  function isSouthBound(t) {
-    // assumes coordinate data is always listed north to south
-    return getStationIndex(coordinates, t.origin) < getStationIndex(coordinates, t.dest);
-  }
-
-  function getStationIndex(line, station) {
-    var i = line.map(function(e) {
-      return e.station;
-    }).indexOf(station);
-    return i;
-  }
-
-  function isOnRed(t) {
-    // is trip object t's origin  == to any of coordinates's stations?
-    for (var i = 0; i < coordinates.length; i++) {
-      if (t.origin == coordinates[i].station) {
-        for (var i = 0; i < coordinates.length; i++) {
-          if (t.dest == coordinates[i].station) {
-            return true;
-          }
-        }
-      }
-    }
-  }
-
-  // sum all riders that board at an origin station that's on a specific line
-  function sumBoard (line, origin) {
-    var ridersCount = line.reduce(function(sum, x) {
-      if (x.origin == origin) {
-        return sum + x.riders;
-      }
-      return sum;
-    }, 1);
-    return ridersCount;
-  }
-
-  // for a set of trips, sum all riders that exit at a specified dest station
-  function sumExits (trips, dest) {
-    var ridersCount = trips.reduce(function(sum, x) {
-      if (x.dest == dest) {
-        return sum + x.riders;
-      }
-      return sum;
-    }, 1);
-    return ridersCount;
-  }
-
-  //filter function to return only trips of a requested hour
-  function tripsHour(hour) {
-    return function(t) {
-      return t.hour == hour;
-    }
-  }
-}
-
+var h = 0;
 function draw() {
-  console.log("draw");
+  console.log("draw hour " + h);
   var line =  d3.line()
     .x(function(d) { return d.x;  })
     .y(function(d) { return d.y; });
@@ -117,7 +49,8 @@ function draw() {
   var temp = [];
   var time = 0;
   var totalTime = 0;
-  var h = 0;
+  
+  
   // filter ridership data to only trips on a specific line (red, blue, green, etc)
   var redLineRiders = riders.filter(isOnRed);
   // filter ridership data to direction NB or SB
@@ -181,26 +114,27 @@ function draw() {
       return t.hour == hour;
     }
   }
+  console.log("trips: " + JSON.stringify(trips));
+
+
+  var trainLoad = 0;
   for(var i = 0; i < coordinates.length - 1; ++i) {
     temp[0] = coordinates[i];
     temp[1] = coordinates[i+1];
     time = 1000;
     var origin = temp[0].station;
     var dest = temp[1].station;
-    var trainLoad = 0;
-
+    trainLoad += sumBoard(trips, origin) - sumExits(trips, dest);
+    console.log(sumBoard(trips, origin) + " get on at " + origin + " and " + sumExits(trips, dest) + " get off at " + dest);
+    console.log("trainLoad: " + trainLoad);
+    
     var paths = svg.append("path")
       .attr("d", line(temp))
       .attr("stroke", "steelblue")
       .attr("stroke-linecap", "round")
       .attr("stroke-width", function(d, index) { 
         // return ridership data for each path
-
-        console.log(sumBoard(trips, origin));
-        trainLoad += sumBoard(trips, origin);
-        trainLoad -= sumExits(trips, dest);
-        console.log("trainLoad: " + trainLoad);
-        return trainLoad / 100;
+        return Math.max(trainLoad / 100, .12);
       })
       .attr("fill", "none");
 
@@ -216,5 +150,10 @@ function draw() {
 
     totalTime += time;
 
+  }
+  // wait for draw() completion
+  if (h < 3) {
+    h++;
+    draw();
   }
 }
