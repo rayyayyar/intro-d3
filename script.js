f86a70a142589280d674ca476a48741f44bedc4c
Ray Luong <rayluong@gmail.com>
2017-02-15 11:49:03 -0800

    animates per path

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Starting a d3 experiment");
 
 d3.select("body").style("background-color", "white");
 var w = 1000,
-		h = 1000;
+    h = 1000;
 
 //append svg to div#line
 var svg = d3.select("#line")
@@ -22,60 +22,85 @@ queue()
     .await(ready);
 
 function ready(error, coordinatesJSON, ridersJSON) {
-	error = error;
-	coordinates = coordinatesJSON;
-	riders = ridersJSON;
-	console.log("coordinates: " + JSON.stringify(coordinates));
-	draw();
+  error = error;
+  coordinates = coordinatesJSON;
+  riders = ridersJSON;
+  console.log("coordinates: " + JSON.stringify(coordinates));
+  draw();
 }
 
 function draw() {
-	console.log("draw");
-	var line = 	d3.line()
-								.x(function(d, i) { console.log("i:" + i); return d.x;  })
-								.y(function(d, i) { return d.y; });
+  console.log("draw");
+  var line =  d3.line()
+                .x(function(d) { return d.x;  })
+                .y(function(d) { return d.y; });
 
-	var circles = d3.select("svg").selectAll("circle")
-		.data(coordinates)
-		.enter()
-		.append("circle")
-		.attr("cx", function(d, i) {
-			return d.x;
-		})
-		.attr("cy", function(d) {
-			return d.y;
-		})
-		.attr("r", 2)
-	  .attr("fill", "steelblue");
+  var circles = d3.select("svg").selectAll("circle")
+    .data(coordinates)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d, i) {
+      return d.x;
+    })
+    .attr("cy", function(d) {
+      return d.y;
+    })
+    .attr("r", 2)
+    .attr("fill", "steelblue");
 
-var paths = d3.select("svg").selectAll("path")
-		.data(coordinates)
-		.enter()
-		.append("path")
-		.attr("fill", "none")
-		.attr("stroke", "steelblue")
-		.attr("stroke-width", function(d, i) { return Math.random() * i });
 
-	
-	paths.attr("d", function(d, i) { if (i <=  paths.data().length - 2) { return line(segmentData(i)); } })
+  var temp = [];
+  var time = 0;
+  var totalTime = 0;
 
-	function segmentData(i) {
-		
-			var pair = [{"x": paths.data()[i].x, "y": paths.data()[i].y}, {"x": paths.data()[i+1].x, "y": paths.data()[i+1].y}];
-			return pair;
-		
-	}
+  for(var i = 0; i < coordinates.length - 1; ++i) {
+    temp[0] = coordinates[i];
+    temp[1] = coordinates[i+1];
+    time = 1000;
 
-	// attempt animation
-	// var totalLength = paths.node().getTotalLength();
+    var paths = svg.append("path")
+      .attr("d", line(temp))
+      .attr("stroke", "steelblue")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", function(d, index) { return Math.random() * 8; })
+      .attr("fill", "none");
 
-	// paths
-	//   .attr("stroke-dasharray", totalLength + " " + totalLength)
-	//   .attr("stroke-dashoffset", totalLength)
-	//   .transition()
-	//     .duration(1000)
-	//     .ease(d3.easeLinear)
-	//     .attr("stroke-dashoffset", 0);
+
+    var totalLength = paths.node().getTotalLength();
+
+    paths.attr("stroke-dasharray", totalLength + " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition()
+        .delay(totalTime)
+        .duration(time)
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0);
+
+    totalTime += time;
+
+  }
+  
+
+  
+  // paths.attr("d", function(d, i) { if (i <=  paths.data().length - 2) { return line(segmentData(i)); } })
+
+  // function segmentData(i) {
+    
+  //     var pair = [{"x": paths.data()[i].x, "y": paths.data()[i].y}, {"x": paths.data()[i+1].x, "y": paths.data()[i+1].y}];
+  //     return pair;
+    
+  // }
+
+  // attempt animation
+  // var totalLength = paths.node().getTotalLength();
+
+  // paths
+  //   .attr("stroke-dasharray", totalLength + " " + totalLength)
+  //   .attr("stroke-dashoffset", totalLength)
+  //   .transition()
+  //     .duration(1000)
+  //     .ease(d3.easeLinear)
+  //     .attr("stroke-dashoffset", 0);
 
 
 }
